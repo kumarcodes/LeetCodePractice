fd82a8d61d33768a6098df443db5750ec8458f9a
Rohit Sharma <merohitks@gmail.com>
2026-04-18 12:34:52 +0530

    Find the Mirror distance between an integer and it's reverse.

---
+++ src/arrays/Mirror distance integer.java
@@ -0,0 +1,19 @@
+public class MirrorDistanceInteger {
+    public static void main(String[] args) {
+        int input = 70;
+        int diff = calculateMirrorDistance(input);
+        System.out.println(diff);
+    }
+
+    private static int calculateMirrorDistance(int input) {
+        int inputStored = input;
+        int reverse = 0;
+        int rem;
+        while (input > 0) {
+            rem = input % 10;
+            reverse = reverse * 10 + rem;
+            input = input / 10;
+        }
+        return Math.abs(inputStored - reverse);
+    }
+}
