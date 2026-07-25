655fb21d03f7c28f1b52037f7e9cc37f346f5dda
Rohit Sharma <merohitks@gmail.com>
2026-07-25 23:57:29 +0530

    Added: Implement second approach to DisappearedNumbersInArray for improved time complexity

--- src/hashtable/DisappearedNumbersInArray.java
+++ src/hashtable/DisappearedNumbersInArray.java
@@ -5,8 +5,12 @@ import java.util.*;
 public class DisappearedNumbersInArray {
     public static void main(String[] args) {
         int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        //Approach 1 brute force
         int[] res = disappearedNumbers(nums);
         System.out.println(Arrays.toString(res));
+        //Approach 2 Better Time Complexity
+        List<Integer> list = disappearedNumbersWithoutMap(nums);
+        System.out.println(list);
     }
 
     private static int[] disappearedNumbers(int[] nums) {
@@ -25,5 +29,19 @@ public class DisappearedNumbersInArray {
             result[i] = list.get(i);
         }
         return result;
+    }
+
+    private static List<Integer> disappearedNumbersWithoutMap(int[] nums) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[nums[i] - 1] = nums[i];
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] == 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
     }
 }
