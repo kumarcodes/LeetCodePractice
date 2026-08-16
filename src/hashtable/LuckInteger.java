2a4713f85950c76b4fcc31cd230ad3f5862f03f4
Rohit Sharma <merohitks@gmail.com>
2026-08-16 14:20:45 +0530

    Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
    Return the largest lucky integer in the array. If there is no lucky integer return -1.

---
+++ src/hashtable/LuckInteger.java
@@ -0,0 +1,27 @@
+package hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class LuckInteger {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 3, 4};
+        int num = luckIntegerFinder(arr);
+        System.out.println(num);
+    }
+
+    private static int luckIntegerFinder(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int i : arr) {
+            countMap.put(i, countMap.getOrDefault(i, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (Objects.equals(entry.getKey(), entry.getValue()))
+                return entry.getKey();
+        }
+        System.out.println(countMap);
+
+        return -1;
+    }
+}
